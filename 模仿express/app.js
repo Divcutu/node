11d13fc7ef7b1869678bf0ad7598a0b08a75beb6
dxx <1i0_iwxpicc67n@dingtalk.com>
2021-01-04 18:35:29 +0800

    封装类似express功能

--- 模仿express/app.js
+++ 模仿express/app.js
@@ -1,23 +1,44 @@
+const url = require('url')
+const path = require('path')
+const fs = require('fs')
 
 // 用来保存ge、post方法的对象,
 let G = {
   GET: {},
-  POST: {}
+  POST: {},
+  static: 'static'
 }
 
-// 创建app
+/**
+ * 创建app
+ * @param {*} req 
+ * @param {*} res 
+ * 
+ * post 传参通过监听 data 数据流来获取
+ * get 传参通过url.parse queryString 参数为true来获取
+ */
 const app = function(req, res) {
-  console.log(G)
 
-  if (G[req.method][req.url]) {
-    G[req.method][req.url](req, res)
+  let URL = url.parse(req.url, true)
+  let _url = URL.pathname
+  req.query = URL.query || {}
+  end(res)
+  
+  if (G[req.method][_url]) {
+    let body = ''
+    req.on('data', data => {
+      body += data
+    })
+    req.on('end', () => {
+      req.body = JSON.parse(body) || ''
+      G[req.method][_url](req, res)
+    })
   } else {
-    console.log(req.url)
-    throw new Error('无法识别的请求')
-
+    static(req, res)
   }
 }
 
+// 注册app 的 get post 方法
 app.get = function(url, callback) {
   if(G.GET[url]) {
     throw new Error('重复注册')
@@ -31,6 +52,32 @@ app.post = function(url, callback) {
   } else {
     G.POST[url] = callback
   }
+}
+
+app.static = function(static){
+  G.static = static
+}
+
+// 返回请求的默认格式
+function end(res){
+  res.writeHead(200, {'charset': 'utf-8', 'Content-Type': 'application/json'})
+}
+
+// 处理静态文件的方法
+// 提前读取到返回格式的文件
+const content = JSON.parse(fs.readFileSync('./configure/content-type.json').toString())
+function static(req, res) {
+  let _path = path.join(G.static, url.parse(req.url).pathname)
+  fs.readFile(`./${_path}`, (err, data) => {
+    if(err) {
+      res.writeHead(404, {'Content-Type': 'application/json', 'charset': 'utf-8'})
+      res.end(JSON.stringify({code: 404, data: {}, message: '未找到对应文件'}))
+    }
+    else {
+      res.writeHead(200, {'Content-Type': `${content[path.extname(_path)]}`, 'charset': 'utf-8'})
+      res.end(data)
+    }
+  })
 }
 
 module.exports = app
--- 模仿express/main.js
+++ 模仿express/main.js
@@ -7,12 +7,18 @@ serve.listen(7000)
 serve.on('request', app)
 
 app.get('/aaa', (req, res) => {
-  let a = url.parse(req.url)
-  console.log(req.url)
-  res.end('aaaa')
+  let { query } = req
+  console.log(query)
+  res.end(JSON.stringify(query))
 })
 app.post('/aaa', (req, res) => {
-  console.log(req.params)
-  res.end('postaaa')
+  console.log(req.body)
+  let ret = JSON.stringify({
+    code: 200,
+    data: {
+      ...req.body
+    },
+    message: '成功'
+  })
+  res.end(ret)
 })
-console.log(app.get)
