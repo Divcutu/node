62c0ccd22fd60d41e9b2c0e215b10b204a9eb5b9
divcutu <1427168220@qq.com>
2025-09-20 00:22:52 +0800

    argon2 数据加密

--- 二次学习/express-demo/controller/index.js
+++ 二次学习/express-demo/controller/index.js
@@ -1,8 +1,9 @@
 /**
  * 用来处理路由的控制器
  */
+import { userModel } from '../model/index.js'
+import argon2  from 'argon2'
 
-import { useModel } from '../model/index.js'
 /**
  * 注册接口处理函数
  * @param {object} req 
@@ -11,9 +12,9 @@ import { useModel } from '../model/index.js'
  */
 export const handleRegister = async (req, res, next) => {
     // const result = await useModel.save(req.body)
-    const user = new useModel(req.body)
+    const password = await argon2.hash(req.body.password)
+    const user = new userModel({ ...req.body, password })
     const ret = await user.save()
-    console.log(ret)
 
     res.send('register')
 }
@@ -24,8 +25,14 @@ export const handleRegister = async (req, res, next) => {
  * @param {*} res 
  * @param {*} next 
  */
-export const handleLogin = (req, res, next) => {
+export const handleLogin = async (req, res, next) => {
 
-    
+    const user = await userModel.findOne({ username: req.body.username })
+    console.log(user)
+    const result = await argon2.verify(user.password, req.body.password)
+    console.log(result, user.password, req.body.password)
+    if (!result) {
+        return res.status(402).json({ code: 402, message: '密码错误'})
+    }
     res.send('login')
 }
--- 二次学习/express-demo/middleWare/userValidator.js
+++ 二次学习/express-demo/middleWare/userValidator.js
@@ -1,6 +1,13 @@
 import { query, body, validationResult } from 'express-validator'
 import { validate } from '../utils/validator.js'
 
+
+export const validateUserRegister = validate([
+    body('username').isLength({ min: 3 }).withMessage('用户名长度不能小于3位'),
+    body('email').isEmail().withMessage('邮箱格式不正确'),
+    body('password').isLength({ min: 6 }).withMessage('密码长度不能小于6位'),
+])
+
 export const validateUserLogin = validate([
     body('username').isLength({ min: 3 }).withMessage('用户名长度不能小于3位'),
     body('email').isEmail().withMessage('邮箱格式不正确'),
